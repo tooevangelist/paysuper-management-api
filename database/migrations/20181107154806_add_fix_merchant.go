644caac5bf130314d1870e4bd7b93f4a6781b533
dmitriy.sinichkin <dmitriy.sinichkin@gmail.com>
2018-11-07 15:50:37 +0300

    PAY-16 fix merchant migration

---
+++ database/migrations/20181107154806_add_fix_merchant.go
@@ -0,0 +1,30 @@
+package migrations
+
+import (
+	"github.com/ProtocolONE/p1pay.api/database/model"
+	"github.com/ProtocolONE/p1pay.api/manager"
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+	"github.com/xakep666/mongo-migrate"
+)
+
+func init() {
+	err := migrate.Register(
+		func(db *mgo.Database) error {
+			m := &model.Merchant{
+				Id: bson.NewObjectId(),
+				ExternalId: "5be2c3022b9bb6000765d132",
+				Email: "dmitriy.sinichkin@protocol.one",
+			}
+
+			return db.C(manager.TableMerchant).Insert(m)
+		},
+		func(db *mgo.Database) error {
+			return db.C(manager.TableMerchant).Remove(bson.M{"external_id": "5be2c3022b9bb6000765d132"})
+		},
+	)
+
+	if err != nil {
+		return
+	}
+}
