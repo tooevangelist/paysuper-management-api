6266866b234ef3db12044eb8bc14635eecb3e9c1
dmitriy.sinichkin <dmitriy.sinichkin@gmail.com>
2018-11-07 17:01:03 +0300

    PAY-16 disable debug

--- manager/order.go
+++ manager/order.go
@@ -372,7 +372,7 @@ func (om *OrderManager) getOrderFixedPackage(c *check) (*geoip2.City, error) {
 	fps, ok := c.project.FixedPackage[region]
 
 	if !ok || len(fps) <= 0 {
-		return nil, errors.New(orderErrorFixedPackageForRegionNotFound + " ==== " + c.order.CreateOrderIp)
+		return nil, errors.New(orderErrorFixedPackageForRegionNotFound)
 	}
 
 	var ofp *model.FixedPackage
