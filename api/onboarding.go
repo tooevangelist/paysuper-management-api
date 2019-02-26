a429e6be2cb5cf4eafa8a4246854943d3cd1b01d
dmitriy.sinichkin <dmitriy.sinichkin@gmail.com>
2019-02-26 11:38:46 +0300

    PAY-0 change path to payment form js library

---
+++ api/onboarding.go
@@ -0,0 +1,53 @@
+package api
+
+import "github.com/labstack/echo"
+
+type onboardingRoute struct {
+	*Api
+}
+
+func (api *Api) initOnboardingRoutes() *Api {
+	route := onboardingRoute{Api: api}
+
+	api.accessRouteGroup.GET("/merchant", route.getMerchant)
+	api.accessRouteGroup.POST("/merchant", route.createMerchant)
+	api.accessRouteGroup.PUT("/merchant", route.updateMerchant)
+
+	return api
+}
+
+func (r *onboardingRoute) getMerchant(ctx echo.Context) error {
+
+}
+
+func (r *onboardingRoute) listMerchants(ctx echo.Context) error {
+
+}
+
+func (r *onboardingRoute) createMerchant(ctx echo.Context) error {
+
+}
+
+func (r *onboardingRoute) updateMerchant(ctx echo.Context) error {
+
+}
+
+func (r *onboardingRoute) getPaymentMethod(ctx echo.Context) error {
+
+}
+
+func (r *onboardingRoute) listPaymentMethods(ctx echo.Context) error {
+
+}
+
+func (r *onboardingRoute) createPaymentMethod(ctx echo.Context) error {
+
+}
+
+func (r *onboardingRoute) updatePaymentMethod(ctx echo.Context) error {
+
+}
+
+func (r *onboardingRoute) deletePaymentMethod(ctx echo.Context) error {
+
+}
