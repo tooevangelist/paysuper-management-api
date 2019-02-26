37e116b08f258af6098b92bdfd6208f1123a2b95
dmitriy.sinichkin <dmitriy.sinichkin@gmail.com>
2019-02-26 11:39:31 +0300

    PAY-0 change path to payment form js library

--- api/onboarding.go
+++ api/onboarding.go
@@ -17,37 +17,37 @@ func (api *Api) initOnboardingRoutes() *Api {
 }
 
 func (r *onboardingRoute) getMerchant(ctx echo.Context) error {
-
+	return nil
 }
 
 func (r *onboardingRoute) listMerchants(ctx echo.Context) error {
-
+	return nil
 }
 
 func (r *onboardingRoute) createMerchant(ctx echo.Context) error {
-
+	return nil
 }
 
 func (r *onboardingRoute) updateMerchant(ctx echo.Context) error {
-
+	return nil
 }
 
 func (r *onboardingRoute) getPaymentMethod(ctx echo.Context) error {
-
+	return nil
 }
 
 func (r *onboardingRoute) listPaymentMethods(ctx echo.Context) error {
-
+	return nil
 }
 
 func (r *onboardingRoute) createPaymentMethod(ctx echo.Context) error {
-
+	return nil
 }
 
 func (r *onboardingRoute) updatePaymentMethod(ctx echo.Context) error {
-
+	return nil
 }
 
 func (r *onboardingRoute) deletePaymentMethod(ctx echo.Context) error {
-
+	return nil
 }
