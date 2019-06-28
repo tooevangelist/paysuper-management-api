6c4369228508337953951221c8be4f0b8469b8fa
dmitriy.sinichkin <dmitriy.sinichkin@gmail.com>
2019-06-28 15:25:32 +0300

    PAY-0 debug

--- api/country.go
+++ api/country.go
@@ -4,6 +4,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/paysuper/paysuper-billing-server/pkg/proto/billing"
 	"github.com/paysuper/paysuper-billing-server/pkg/proto/grpc"
+	"log"
 	"net/http"
 )
 
@@ -28,6 +29,7 @@ func (cApiV1 *CountryApiV1) get(ctx echo.Context) error {
 
 	res, err := cApiV1.billingService.GetCountriesList(ctx.Request().Context(), &grpc.EmptyRequest{})
 	if err != nil {
+		log.Println(err)
 		return echo.NewHTTPError(http.StatusInternalServerError, errorCountriesListError)
 	}
 
