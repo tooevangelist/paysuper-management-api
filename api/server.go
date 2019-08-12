e26cade6242514bc7c07e3c66695ef0392e3ee4c
dmitriy.sinichkin <dmitriy.sinichkin@gmail.com>
2019-08-12 09:47:16 +0300

    PAY-377 get signature request logic refactoring

--- api/server.go
+++ api/server.go
@@ -162,7 +162,7 @@ func NewServer(p *ServerInitParams) (*Api, error) {
 	api.accessRouteGroup.Use(middleware.Recover())
 
 	api.authUserRouteGroup = api.Http.Group(apiAuthUserGroupPath)
-	/*api.authUserRouteGroup.Use(
+	api.authUserRouteGroup.Use(
 		jwtMiddleware.AuthOneJwtCallableWithConfig(
 			api.jwtVerifier,
 			func(ui *jwtverifier.UserInfo) {
@@ -175,11 +175,7 @@ func NewServer(p *ServerInitParams) (*Api, error) {
 			},
 		),
 	)
-	api.authUserRouteGroup.Use(api.getUserDetailsMiddleware)*/
-	api.authUser = &AuthUser{
-		Id:    "ffffffffffffffffffffffff",
-		Email: "dmitriy.sinichkin@gmail.com",
-	}
+	api.authUserRouteGroup.Use(api.getUserDetailsMiddleware)
 
 	api.webhookRouteGroup = api.Http.Group(apiWebHookGroupPath)
 	api.webhookRouteGroup.Use(middleware.BodyDump(func(ctx echo.Context, reqBody, resBody []byte) {
