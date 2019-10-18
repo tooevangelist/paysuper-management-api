19847b7ec50ce6e1b2074f44194922526556fd8b
Ilya Krasheninnikov <merlin@nerufa.ru>
2019-10-18 13:36:00 +0300

    MerchantId binder for merchant URLs

--- internal/dispatcher/common/binder.go
+++ internal/dispatcher/common/binder.go
@@ -12,7 +12,9 @@ import (
 	"github.com/paysuper/paysuper-billing-server/pkg/proto/billing"
 	"github.com/paysuper/paysuper-billing-server/pkg/proto/grpc"
 	"io/ioutil"
+	"reflect"
 	"strconv"
+	"strings"
 )
 
 var (
@@ -21,6 +23,8 @@ var (
 	BinderDefault         = &Binder{}
 	EchoBinderDefault     = &echo.DefaultBinder{}
 )
+
+const MerchantIdField = "MerchantId"
 
 type SystemBinder struct{}
 
@@ -31,6 +35,38 @@ func (b *SystemBinder) Bind(i interface{}, ctx echo.Context) (err error) {
 type MerchantBinder struct{}
 
 func (b *MerchantBinder) Bind(i interface{}, ctx echo.Context) (err error) {
+
+	rv := reflect.ValueOf(i)
+
+	if rv.Type().Kind() != reflect.Ptr || rv.IsNil() {
+		return ErrorInternal
+	}
+
+	irv := rv.Elem()
+	irt := irv.Type()
+
+	if irt.Kind() != reflect.Struct {
+		return nil
+	}
+
+	for i := 0; i < irv.NumField(); i++ {
+
+		rv := irv.Field(i)
+		tf := irt.Field(i)
+
+		if strings.EqualFold(tf.Name, MerchantIdField) {
+
+			if tf.Type.Kind() != reflect.String {
+				return ErrorInternal
+			}
+
+			u := ExtractUserContext(ctx)
+			rv.Set(reflect.ValueOf(u.MerchantId))
+
+			break
+		}
+	}
+
 	return nil
 }
 
