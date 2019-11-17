afa03019ad4548c13664b92aed86c331ab8d2c2d
Ilya Krasheninnikov <merlin@nerufa.ru>
2019-11-17 19:51:27 +0300

    Clean up

--- internal/handlers/onboarding.go
+++ internal/handlers/onboarding.go
@@ -890,7 +890,6 @@ func (h *OnboardingRoute) changeMerchantManualPayout(ctx echo.Context, enableMan
 }
 
 func (h *OnboardingRoute) setOperatingCompany(ctx echo.Context) error {
-
 	req := &grpc.SetMerchantOperatingCompanyRequest{}
 	err := ctx.Bind(req)
 
